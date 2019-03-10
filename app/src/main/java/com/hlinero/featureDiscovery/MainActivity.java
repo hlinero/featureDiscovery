9a91b400ce7b3320ec1e7e189cf516030d4032a5
Humberto Linero <humberto_1992@msn.com>
2019-03-10 19:22:06 +0100

    setting the project

--- app/src/main/java/com/hlinero/featureDiscovery/MainActivity.java
+++ app/src/main/java/com/hlinero/featureDiscovery/MainActivity.java
@@ -1,13 +1,112 @@
 package com.hlinero.featureDiscovery;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.content.DialogInterface;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.Display;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
+import com.getkeepsafe.taptargetview.TapTargetView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private final static String TAG = MainActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing the toolbar
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // Inflating the toolbar menu
+        toolbar.inflateMenu(R.menu.menu_main);
+
+        // Setting the back arrow in the toolbar
+        toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_white_24dp));
+
+        // Creating a target sequence
+        final TapTargetSequence sequence = new TapTargetSequence(this)
+                .targets(
+
+                        // Target #1: Back Arrow --> We need to pass the toolbar
+                        TapTarget
+                                .forToolbarNavigationIcon(toolbar, "This is the back button", "It allows you to go back")
+                                .cancelable(false),
+
+                        // Target #2: Search Menu Item --> We have to pass the toolbar and the id of the menu item to target.
+                        TapTarget
+                                .forToolbarMenuItem(toolbar, R.id.search, "This is a search icon", "It let's you search for things")
+                                .cancelable(false),
+
+                        // Target #3: Overflow Menu Item --> We have to pass the toolbar and the id of the menu item to target.
+                        TapTarget
+                                .forToolbarOverflow(toolbar, "This shows you more options", "These are not that important")
+                                .cancelable(false)
+                )
+                .listener(new TapTargetSequence.Listener() {
+                    @Override
+                    public void onSequenceFinish() {
+
+                        // Notify the user the feature discovery sequence is done
+                        Toast.makeText(MainActivity.this,"Congrats! You're all set.",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+
+                        // Logging the id of the target clicked
+                        Log.d(TAG, "Clicked on " + lastTarget.id());
+                    }
+
+                    @Override
+                    public void onSequenceCanceled(TapTarget lastTarget) {
+
+                        // Notify the user he has canceled the sequence
+                        // This only happens if .cancelable(false) is not set
+                        Toast.makeText(MainActivity.this,"You canceled the sequence :(",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Instead of a sequence, we ca have a single time tap target
+        TapTargetView
+                .showFor(this, TapTarget.forView(findViewById(R.id.fab), "Hi there!", "This is a tutorial for feature discovery. ")
+                .cancelable(false)
+                .tintTarget(false), new TapTargetView.Listener() {
+            @Override
+            public void onTargetClick(TapTargetView view) {
+                super.onTargetClick(view);
+
+                // Start the sequence of item
+                sequence.start();
+            }
+
+            @Override
+            public void onOuterCircleClick(TapTargetView view) {
+                super.onOuterCircleClick(view);
+
+                // Listening to clicks in the outer circle
+                Toast.makeText(view.getContext(), "This is the outer circle", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTargetDismissed(TapTargetView view, boolean userInitiated) {
+                Log.d(TAG, "You dismissed me");
+            }
+        });
     }
 }
